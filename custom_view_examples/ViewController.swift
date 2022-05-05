96ed0a3ad18af7a00481ed04d18950950e20b4f7
Ahmet Hudayberdyyev <ahmethudayberdyyev@Ahmets-iMac.local>
2022-05-05 15:43:17 +0500

    first commit

---
+++ custom_view_examples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  custom_view_examples
+//
+//  Created by Ahmet Hudayberdyyev on 5/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
