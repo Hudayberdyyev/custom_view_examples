8082888970affa26ac0f0c495ac83eed4171fadd
Ahmet Hudayberdyyev <ahmethudayberdyyev@Ahmets-iMac.local>
2022-05-06 16:00:16 +0500

    constants refactoring

--- custom_view_examples/Constants/Constants.swift
+++ custom_view_examples/Constants/Constants.swift
@@ -10,6 +10,6 @@ import UIKit
 struct Constants {
     struct basicMovieInfoView {
         static let titleFont: CGFloat = CGFloat(0.022.screenMaxDimensionRelativeValue)
-        static let descFont: CGFloat = CGFloat(0.017.screenMaxDimensionRelativeValue)
+        static let baseInfoFont: CGFloat = CGFloat(0.017.screenMaxDimensionRelativeValue)
     }
 }
