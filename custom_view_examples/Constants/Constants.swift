977f08acb3a12309512f39481372160aa9ed3c33
Ahmet Hudayberdyyev <ahmethudayberdyyev@Ahmets-iMac.local>
2022-05-07 09:07:36 +0500

    add some padding to labels in filmHighlights

--- custom_view_examples/Constants/Constants.swift
+++ custom_view_examples/Constants/Constants.swift
@@ -11,6 +11,8 @@ struct Constants {
     struct basicMovieInfoView {
         static let titleFont: CGFloat = CGFloat(0.022.screenMaxDimensionRelativeValue)
         static let baseInfoFont: CGFloat = CGFloat(0.020.screenMaxDimensionRelativeValue)
+        static let filmHighlightsHorizontalEdgeInsets: CGFloat = 5
+        static let filmHighlightsVerticalEdgeInsets: CGFloat = 3
     }
 }
 
--- custom_view_examples/MainInfoView/BasicInfoView/BasicInfoViewSubviews/KPRatingView/KPRatingSubviews/KPRatingLabel.swift
+++ custom_view_examples/MainInfoView/BasicInfoView/BasicInfoViewSubviews/KPRatingView/KPRatingSubviews/KPRatingLabel.swift
@@ -7,7 +7,31 @@
 
 import UIKit
 
-class KPRatingLabel: UILabel {
+@IBDesignable class KPRatingLabel: UILabel {
+    
+    //MARK: -EdgeInsets
+    @IBInspectable var topInset: CGFloat = Constants.basicMovieInfoView.filmHighlightsVerticalEdgeInsets
+    @IBInspectable var bottomInset: CGFloat = Constants.basicMovieInfoView.filmHighlightsVerticalEdgeInsets
+    @IBInspectable var leftInset: CGFloat = Constants.basicMovieInfoView.filmHighlightsHorizontalEdgeInsets
+    @IBInspectable var rightInset: CGFloat = Constants.basicMovieInfoView.filmHighlightsHorizontalEdgeInsets
+    
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + leftInset + rightInset,
+                      height: size.height + topInset + bottomInset)
+    }
+    
+    override var bounds: CGRect {
+        didSet {
+            // ensures this works within stack views if multi-line
+            preferredMaxLayoutWidth = bounds.width - (leftInset + rightInset)
+        }
+    }
     
     //MARK: -Initializers
     private func initialize() {
