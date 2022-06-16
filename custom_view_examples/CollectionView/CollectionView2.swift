e674c6d3a02c1531dd21731b375c494293a1b53c
Ahmet Hudayberdyyev <hudayberdyyev.ahmet@gmail.com>
2022-06-16 16:52:00 +0500

    some refactoring

---
+++ custom_view_examples/CollectionView/CollectionView2.swift
@@ -0,0 +1,37 @@
+//
+//  CollectionView2.swift
+//  custom_view_examples
+//
+//  Created by Ahmet Hudayberdyyev on 6/16/22.
+//
+
+import UIKit
+
+class CustomSuperView: UIView {
+    private func initialize() {
+        print("\(#fileID) => \(#function)")
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemBlue
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        print("\(#fileID) => \(#function)")
+        initialize()
+        setupUI()
+    }
+    
+    @available (*, unavailable)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    @available (*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setupUI() {
+        print("\(#fileID) => \(#function)")
+    }
+}
